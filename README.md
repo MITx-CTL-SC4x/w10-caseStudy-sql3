746062f56229cc8d61f6adb2e0f33a0bfdd52fca
Kellen Betts <kellen.betts@gmail.com>
2025-03-04 18:09:21 -0800

    adding template code and customizing for the problem

---
+++ README.md
@@ -0,0 +1,69 @@
+# SC4x | Week 10 | Case Study | SQL 3
+
+## Overview
+The app evaluates and provides feedback on a single SQL query using an AI API (OpenAI, Gemini, or Claude).
+
+## Prerequisites
+- Python 3.6 or later
+- pip
+- virtualenv (optional but recommended)
+- OpenAI, Claude, and/or Google Gemini API key(s)
+
+## Local Setup Instructions
+
+Navigate to a location where you'd like to run this app from, and clone the repo:
+
+```bash
+git clone https://github.com/MITx-CTL-SC4x/w10-caseStudy-sql3.git
+```
+
+Navigate to your project directory in the terminal:
+```bash
+cd w10-caseStudy-sql3
+```
+
+## Running the App Locally
+
+### 1. Create a Virtual Environment
+
+It's recommended to isolate the project's dependencies using a virtual environment. You can utilize tools like venv or virtualenv to achieve this. Refer to official documentation for specific commands based on your chosen tool. Here is the command from a mac shell:
+```bash
+python3 -m venv venv
+```
+
+Finally, activate your virtual environment:
+```bash
+source venv/bin/activate
+```
+
+### 2. Install Requirements
+Activate your virtual environment and install the required packages using pip:
+```bash
+pip3 install -r requirements.txt
+```
+
+### 3. Running the App
+
+API key(s): Set your API keys in .env ... see .env_sample
+
+Navigate to your project directory in the terminal and execute the following command to launch the Streamlit app:
+```bash
+streamlit run main.py
+```
+
+This will open the App in your web browser, typically at http://localhost:8501.
+
+## Deploying on Streamlit for the First Time
+
+1. Sign into share.streamlit.io
+2. Click 'Deploy an app' and then paste in your GitHub URL
+3. Put API keys in Advanced Settings (see instructions below)
+
+Instructions for API key management are at https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
+
+### Updating the Streamlit App
+
+After changes are committed to the repository, reboot the app from the Streamlit dashboard at https://share.streamlit.io/
+
+## Credit
+This app is based on the 'AI MicroApp (Assistant)' by John Swope found at https://github.com/jswope00/AI-Microapp-Template-Assistant/tree/main
---
+++ config.py
@@ -0,0 +1,134 @@
+########  GENERAL APP INFORMATION  ##############
+
+APP_TITLE = None
+# APP_TITLE = "SC4x | Week 10 | Case Study | SQL 3"
+
+APP_INTRO = None
+# APP_INTRO = """The app evaluates and provides feedback on a single SQL query using an AI API (OpenAI, Gemini, or Claude)."""
+
+APP_HOW_IT_WORKS = None
+# APP_HOW_IT_WORKS = """ """
+
+SHARED_ASSET = {}
+# SHARED_ASSET = {
+#     "name":"NAME",
+#     "path":"FILE.pdf",
+#     "button_text":"Read this PDF first"
+# }
+
+HTML_BUTTON = {}
+
+COMPLETION_MESSAGE = "Thank you for submitting a response!"
+COMPLETION_CELEBRATION = False
+
+SCORING_DEBUG_MODE = False
+
+ ####### PHASES INFORMATION #########
+
+PHASES = {
+    "attempt1": {
+        "type": "text_area",
+        "height": 200,
+        "label": """What product category has the highest average number of items in an order where the delivery was late for customers in either 'rio de janeiro' or 'sao paulo'?""",
+        "instructions": """ For this question, the students where asked to write a query to find the product category that has the highest average number of items in an order (average of order_item_id) where the delivery was late (late_delivery=1) for customers in either 'rio de janeiro' or 'sao paulo'. The correct SQL query for this question:
+    SELECT Products.product_category_name, AVG(OrderItems.order_item_id)
+    FROM Orders, OrderItems, Customers, Products
+    WHERE Orders.order_id = OrderItems.order_id
+      AND Customers.customer_id = Orders.customer_id
+      AND OrderItems.product_id = Products.product_id
+      AND Orders.late_delivery = 1
+      AND (Customers.customer_city = 'rio de janeiro' OR Customers.customer_city = 'sao paulo')
+    GROUP BY Products.product_category_name
+    ORDER BY AVG(OrderItems.order_item_id) DESC;
+Provide compare the following student submission with the correct answer above. Please provide feedback on the student submission if their solution is not correct. Do not provide the correct answer. Instead, provide guidance to help the student identify where they might be missing something in their code. There might be slight variations to the query that will give the correct answer. """,
+        "value": " ",
+        "scored_phase": False,
+        "rubric": """
+            None
+        """,
+        "minimum_score": 2
+    },
+    "attempt2": {
+        "type": "text_area",
+        "height": 200,
+        "label": """Do you want to try again?""",
+        "instructions": """ For this question, the students where asked to write a query to find the product category that has the highest average number of items in an order (average of order_item_id) where the delivery was late (late_delivery=1) for customers in either 'rio de janeiro' or 'sao paulo'. The correct SQL query for this question:
+    SELECT Products.product_category_name, AVG(OrderItems.order_item_id)
+    FROM Orders, OrderItems, Customers, Products
+    WHERE Orders.order_id = OrderItems.order_id
+      AND Customers.customer_id = Orders.customer_id
+      AND OrderItems.product_id = Products.product_id
+      AND Orders.late_delivery = 1
+      AND (Customers.customer_city = 'rio de janeiro' OR Customers.customer_city = 'sao paulo')
+    GROUP BY Products.product_category_name
+    ORDER BY AVG(OrderItems.order_item_id) DESC;
+Provide compare the following student submission with the correct answer above. Please provide feedback on the student submission if their solution is not correct. Do not provide the correct answer. Instead, provide guidance to help the student identify where they might be missing something in their code. There might be slight variations to the query that will give the correct answer. """,
+        "value": " ",
+        "scored_phase": False,
+        "rubric": """
+            None
+        """,
+        "minimum_score": 2
+    },
+    "attempt3": {
+        "type": "text_area",
+        "height": 200,
+        "label": """Do you want to try one more time?""",
+        "instructions": """ For this question, the students where asked to write a query to find the product category that has the highest average number of items in an order (average of order_item_id) where the delivery was late (late_delivery=1) for customers in either 'rio de janeiro' or 'sao paulo'. The correct SQL query for this question:
+    SELECT Products.product_category_name, AVG(OrderItems.order_item_id)
+    FROM Orders, OrderItems, Customers, Products
+    WHERE Orders.order_id = OrderItems.order_id
+      AND Customers.customer_id = Orders.customer_id
+      AND OrderItems.product_id = Products.product_id
+      AND Orders.late_delivery = 1
+      AND (Customers.customer_city = 'rio de janeiro' OR Customers.customer_city = 'sao paulo')
+    GROUP BY Products.product_category_name
+    ORDER BY AVG(OrderItems.order_item_id) DESC;
+Provide compare the following student submission with the correct answer above. Please provide feedback on the student submission if their solution is not correct. Do not provide the correct answer. Instead, provide guidance to help the student identify where they might be missing something in their code. There might be slight variations to the query that will give the correct answer. """,
+        "value": " ",
+        "scored_phase": False,
+        "rubric": """
+            None
+        """,
+        "minimum_score": 2
+    },
+}
+
+######## AI CONFIGURATION #############
+
+########## AI ASSISTANT CONFIGURATION #######
+ASSISTANT_NAME = "sc4x_wk10_CaseStudy_SQL"
+ASSISTANT_INSTRUCTIONS = """ You are an expert in SQL and overseeing a course where students are learning the basics of SQL. """
+
+LLM_CONFIGURATION = {
+    "gpt-4-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4-turbo",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.01,
+        "price_per_1k_completion_tokens": .03
+    },
+    "gpt-4o":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4o",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.0025,
+        "price_per_1k_completion_tokens": .010
+    },
+    "gpt-3.5-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-3.5-turbo-0125",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":0.0005,
+        "price_per_1k_completion_tokens": 0.0015
+    }
+}
+
+ASSISTANT_THREAD = ""
+ASSISTANT_ID_FILE = "assistant_id.txt"
